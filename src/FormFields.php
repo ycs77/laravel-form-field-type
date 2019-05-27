61b109152bfeccb314c038d3173cb1aea57844af
Lucas Yang <yangchenshin77@gmail.com>
2019-05-27 19:55:29 +0800

    Injects data when FormFields is initialized.

--- src/FormFields.php
+++ src/FormFields.php
@@ -5,6 +5,23 @@ namespace Ycs77\LaravelFormFieldType;
 abstract class FormFields
 {
     /**
+     * Form data.
+     *
+     * @var array|null
+     */
+    protected $data;
+
+    /**
+     * Create form fields instance.
+     *
+     * @param array|null  $data
+     */
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+    }
+
+    /**
      * Return form fields array.
      *
      * @return array
